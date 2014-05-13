ea5f8dea04de052dcd54006066b8dded0dd8d513
Hayk Saribekyan <hayks@mit.edu>
2014-05-13 17:00:17 -0400

    Add location in calendar.

--- src/edu/mit/openschedule/ui/CalendarFragment.java
+++ src/edu/mit/openschedule/ui/CalendarFragment.java
@@ -125,7 +125,7 @@ public class CalendarFragment extends Fragment {
 				button.setGravity(Gravity.TOP);
 				if (!meetingNamePosition.containsKey(meeting) ||
 						meetingNamePosition.get(meeting) == position) {
-					button.setText(meeting.getSubject().getNumber() + " " + meeting.getTypeString());
+					button.setText(meeting.getSubject().getNumber() + " " + meeting.getTypeString() + " (" + meeting.getLocationString() + ")");
 					meetingNamePosition.put(meeting, position);
 				}
 				if (!meetingColor.containsKey(meeting)) {
